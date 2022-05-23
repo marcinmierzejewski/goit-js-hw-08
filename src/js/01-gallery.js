317308af7a641fd4e8c37d17f6221643347780fd
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-05-23 23:15:24 +0200

    done task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (img) =>
+      `<a class="gallery__item" href="${img.original}">       
+        <img 
+          class="gallery__image" 
+          src=${img.preview}          
+          alt="${img.description}"
+          >
+      </a>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
 console.log(galleryItems);
+
+let lightbox = new SimpleLightbox(".gallery a", { 
+  captionsData: "alt",
+  captionDelay: 250,
+});
