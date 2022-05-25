c989646db9bfd02110a2e36468d188bdcb6b3e00
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-05-25 21:40:33 +0200

    added throttle method

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,36 +1,22 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
+player.on('play', function () {
+  console.log(`Resume on: ${localStorage.getItem('videoplayer-current-time')} s.`);
 });
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
+player.getVideoTitle().then(function (title) {
+  console.log('Title:', title);
 });
 
-let getCurrentTime = localStorage.getItem("videoplayer-current-time")
-const onPlay = function({seconds}) {
-  localStorage.setItem("videoplayer-current-time", seconds);
-  getCurrentTime = localStorage.getItem("videoplayer-current-time")
-  console.log(`videoplayer-current-time: ${getCurrentTime}`)
-};
+function saveCurrentTime(e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+  console.log(`videoplayer-current-time: ${localStorage.getItem('videoplayer-current-time')}`);
+}
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
 
-
-player.setCurrentTime(getCurrentTime).then(function() {
-  console.log(`Resume on: ${getCurrentTime} s.`)
-}).catch(function(error) {
-  switch (error.name) {
-      case 'RangeError':
-          // the time was less than 0 or greater than the video’s duration
-          break;
-
-      default:
-          // some other error occurred
-          break;
-  }
-});
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
